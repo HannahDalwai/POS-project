84d3e9d61e0d317c02b6d873d21787486fcb6f26
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-17 11:55:24 +0200

    working

--- cart.js
+++ cart.js
@@ -1,106 +1,74 @@
-let cart = JSON.parse(localStorage.getItem("cart"))
-  ? JSON.parse(localStorage.getItem("cart"))
-  : [];
+let cart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [];
 
-// READ
-console.log(cart);
-function readCart(cart) {
-  document.querySelector("#cart").innerHTML = "";
+function showcart(cart){
+    document.querySelector("#cart").innerHTML = "";
 
-  let total = cart
-    .reduce((total, product) => {
-      return total + product.price * product.qty;
-    }, 0)
-    .toFixed(2);
+    cart.forEach((cart, index) => {
+        document.querySelector("#cart").innerHTML += `
 
-  cart.forEach((product, position) => {
-    document.querySelector("#cart").innerHTML += `
-      <div class="card mb-3 w-100  position-relative" >
-      <button type="button" class="position-absolute top-0 start-100 translate-middle badge btn btn-danger" onclick="removeFromCart(${position})">X</button>
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${product.img}" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body d-flex flex-column container">
-              <h5 class="card-title mb-3">${product.title}</h5>
-              <div class="d-flex mb-3 justify-content-between">
-                <p class="card-text">Individual price: </p>
-                <span>R${product.price}<span>
-              </div>
-              <div class="d-flex mb-3 justify-content-between">
-                <label class="form-label">Quantity:</label>
-                <input type="number" min=1 id="remove${position}" value=${
-      product.qty
-    } onchange="updateCart(${position})" />
-              </div>
-              <div class="card-footer bg-white d-flex justify-content-between  p-0 pt-3">
-                <p>Total Cost: </p>
-                <span>R${(
-                  parseFloat(product.price) * parseInt(product.qty)
-                ).toFixed(2)}</span>
-              </div>
-            </div>  
-          </div>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img id="img" src="${cart.img}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title" id="title">${cart.title}</h5>
+                    <p class="card-text" id="price">R${cart.price}</p>
+                    <div>
+                        <input type="number" name="Quantity" class="form-control mb-2" id="quantity${index}" min="1" value="${cart.qty}" onchange="updateCart(${index})">
+                    <div/>
+                    <p>Total = R${parseInt(cart.qty * cart.price)}</p>
+                    <button class="btn btn-danger mt-2" onclick="removeProduct(${index})">Remove</button>
+                </div>
+                </div>
+            </div>
         </div>
-      </div>
-    `;
-  });
-  showCartBadge();
-  document.querySelector("#cart-footer").innerHTML += `
-    <h3>Total cost: R${total}</h3>
-    <button class="btn btn-primary btn-lg" onclick="checkout()">
-      Checkout
-    </button>
-  `;
+
+        `;
+    });
+
+    document.querySelector("#cart").innerHTML += `
+        <h2>Total = R${calculateTotal()}</h2>
+
+            <button type="button" class="btn btn-warning btn-lg mb-4" onclick="checkout()">Checkout</button>
+        
+    
+        `;
+    
 }
 
-// Update Cart Badge
-function showCartBadge() {
-  document.querySelector("#badge").innerHTML = cart ? cart.length : "";
-}
+showcart(cart);
 
-readCart(cart);
 
-// UPDATE
-function updateCart(position) {
-  let qty = document.querySelector(`#remove${position}`).value;
-  cart[position] = { ...cart[position], qty };
-  localStorage.setItem("cart", JSON.stringify(cart));
-  readCart(cart);
-}
+function updateCart(index){
+    let qty = document.querySelector(`#quantity${index}`).value;
 
-// REMOVE
-function removeFromCart(position) {
-  let confirmation = confirm(
-    "Are you sure you want to remove this product from the cart?"
-  );
-
-  if (confirmation) {
-    cart.splice(position, 1);
+    cart[index] = { ...cart[index], qty};
     localStorage.setItem("cart", JSON.stringify(cart));
-    readCart(cart);
-  }
+    showcart(cart);
 }
 
-// CHECKOUT
-function checkout() {
-  let total = cart
-    .reduce((total, product) => {
-      return total + product.price * product.qty;
-    }, 0)
-    .toFixed(2);
-  try {
-    if (parseInt(total) == 0) throw new Error("Nothing in cart");
-    let confirmation = confirm(`Total payment needed: R${total}`);
+function removeProduct(index){
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    showcart(cart);
+}
 
-    if (confirmation) {
-      cart.length = 0;
-      localStorage.removeItem("cart");
-      readCart(cart);
+function calculateTotal(){
+    let total = 0;
+    cart.forEach((product => {
+        total = total + product.price * product.qty;
+    }))
+    return total;
+}
+
+function checkout(){
+    let text = `Your total amount is R${calculateTotal()}.\nAre you sure you want to checkout?`;
+    if(calculateTotal() == 0){
+        alert("You didn't add anything to cart")
+    }else if (confirm(text) == true) {
+        localStorage.removeItem("cart");
+        location.reload();
     }
-  } catch (err) {
-    alert(err);
-  }
 }
-
